d2972d554cca9dcaf8d738023e03605d8509039d
Bendas <rayushekrin@gmail.com>
2025-07-24 18:28:36 +0200

    папка home38

---
+++ home38/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button text="Натисни" onClick={() => alert("Молодець!")} />
+    </div>
+  );
+};
+
+export default App;
